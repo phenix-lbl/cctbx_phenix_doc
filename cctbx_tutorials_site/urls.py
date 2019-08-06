ca28031b4e88a410a5481a2e46b5b82592cdd1fa
Billy K. Poon <bkpoon@lbl.gov>
2019-08-06 12:42:03 -0700

    Update URL paths for server

--- cctbx_tutorials_site/urls.py
+++ cctbx_tutorials_site/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tutorials_cctbx/', include('tutorials_cctbx.urls')),
+    url(r'^', include('tutorials_cctbx.urls')),
 ]
--- tutorials_cctbx/urls.py
+++ tutorials_cctbx/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path
+from django.conf.urls import url
+#from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('melk_2019_1', views.melk_2019_1, name='melk_2019_1'),
-    path('melk_2019_2', views.melk_2019_2, name='melk_2019_2'),
-    path('melk_2019_3', views.melk_2019_3, name='melk_2019_3'),
-    path('melk_2019_4', views.melk_2019_4, name='melk_2019_4'),
-    path('melk_2019_5', views.melk_2019_5, name='melk_2019_5'),
-    path('melk_2019_intro', views.melk_2019_intro, name='melk_2019_intro'),
+    url(r'^$', views.index, name='index'),
+    url(r'^melk_2019_1', views.melk_2019_1, name='melk_2019_1'),
+    url(r'^melk_2019_2', views.melk_2019_2, name='melk_2019_2'),
+    url(r'^melk_2019_3', views.melk_2019_3, name='melk_2019_3'),
+    url(r'^melk_2019_4', views.melk_2019_4, name='melk_2019_4'),
+    url(r'^melk_2019_5', views.melk_2019_5, name='melk_2019_5'),
+    url(r'^melk_2019_intro', views.melk_2019_intro, name='melk_2019_intro'),
 ]
