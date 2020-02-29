c2f79e39c462c67a9e340c9d9c5010f9cabfadeb
dcliebschner <dcliebschner@lbl.gov>
2020-02-28 17:08:02 -0800

    test

--- tutorials_cctbx/urls.py
+++ tutorials_cctbx/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^flex_array', views.flex_array, name='flex_array'),
-    url(r'^tutorials', views.tutorials, name='tutorials'),
+    url(r'^tutorials', views.tutorials, name='tutorials_intro'),
     url(r'^melk_2019_intro', views.melk_2019_intro, name='melk_2019_intro'),
 ]
