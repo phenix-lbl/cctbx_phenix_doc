f78dfdbe0fd1050adad03615db20be8af62896a1
dcliebschner <dcliebschner@lbl.gov>
2020-02-28 16:44:23 -0800

    update urls

--- tutorials_cctbx/urls.py
+++ tutorials_cctbx/urls.py
@@ -7,10 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^flex_array', views.flex_array, name='flex_array'),
     url(r'^tutorials', views.tutorials, name='tutorials'),
-    url(r'^melk_2019_1', views.melk_2019_1, name='melk_2019_1'),
-    url(r'^melk_2019_2', views.melk_2019_2, name='melk_2019_2'),
-    url(r'^melk_2019_3', views.melk_2019_3, name='melk_2019_3'),
-    url(r'^melk_2019_4', views.melk_2019_4, name='melk_2019_4'),
-    url(r'^melk_2019_5', views.melk_2019_5, name='melk_2019_5'),
     url(r'^melk_2019_intro', views.melk_2019_intro, name='melk_2019_intro'),
 ]
