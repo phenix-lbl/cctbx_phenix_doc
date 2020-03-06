80bc089542e085bc011123fb5d1e6c335f597895
dcliebschner <dcliebschner@lbl.gov>
2020-03-06 14:11:40 -0800

    Rearrange

--- tutorials_cctbx/urls.py
+++ tutorials_cctbx/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^flex_array', views.flex_array, name='flex_array'),
     url(r'^tutorials', views.tutorials, name='tutorials_intro'),
-    url(r'^melk_2019_intro', views.melk_2019_intro, name='melk_2019_intro'),
+    url(r'^download', views.download, name='download'),
+    url(r'^cctbx_tour', views.cctbx_tour, name='cctbx_tour'),
+    url(r'^flex_array', views.flex_array, name='flex_array'),
+    url(r'^melk_2019', views.melk_2019, name='melk_2019'),
 ]
--- tutorials_cctbx/views.py
+++ tutorials_cctbx/views.py
@@ -6,13 +6,19 @@ def index(request):
   return render(request, 'tutorials_cctbx/index.html')
   #return HttpResponse("Hello, world. You're at the polls index.")
 
+def download(request):
+  return render(request, 'tutorials_cctbx/download.html')
+
+def cctbx_tour(request):
+  return render(request, 'tutorials_cctbx/cctbx_tour.html')
+
 def tutorials(request):
   args = {'is_active': 'display: block;'}
   return render(request, 'tutorials_cctbx/tutorials/tutorials_intro.html', args)
 
-def melk_2019_intro(request):
+def melk_2019(request):
   args = {'is_active': 'display: block;'}
-  return render(request, 'tutorials_cctbx/melk_2019/melk_2019_intro.html', args)
+  return render(request, 'tutorials_cctbx/tutorials/melk_2019.html', args)
 
 def flex_array(request):
   args = {'is_active_doc': 'display: block;'}
