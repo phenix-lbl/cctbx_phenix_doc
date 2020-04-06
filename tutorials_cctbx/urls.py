29fcb863bbf1b5981503843bdfd7e7b9d8bc7876
dcliebschner <dcliebschner@lbl.gov>
2020-04-06 10:28:50 -0700

    adapt views

--- tutorials_cctbx/urls.py
+++ tutorials_cctbx/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^cctbx_tour', views.cctbx_tour, name='cctbx_tour'),
     url(r'^documentation_overview', views.documentation_overview, name='documentation_overview'),
     url(r'^doc_flex_array', views.doc_flex_array, name='doc_flex_array'),
+    url(r'^doc_phil', views.doc_flex_array, name='doc_phil'),
     url(r'^tutorials', views.tutorials, name='tutorials_intro'),
     url(r'^tuto_melk_2019', views.tuto_melk_2019, name='tuto_melk_2019'),
     url(r'^tuto_rigid_body', views.tuto_rigid_body, name='tuto_rigid_body'),
--- tutorials_cctbx/views.py
+++ tutorials_cctbx/views.py
@@ -35,6 +35,10 @@ def doc_flex_array(request):
   args = {'is_active_doc': 'display: block;'}
   return render(request, 'tutorials_cctbx/documentation/doc_flex_array.html', args)
 
+def doc_phil(request):
+  args = {'is_active_doc': 'display: block;'}
+  return render(request, 'tutorials_cctbx/documentation/doc_phil.html', args)
+
 def newsletter_artcls(request):
   return render(request, 'tutorials_cctbx/newsletter_artcls.html')
 
