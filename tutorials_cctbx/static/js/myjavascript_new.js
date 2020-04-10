4c7d98651d374032ce2bda309deb4617c524d4d9
dcliebschner <dcliebschner@lbl.gov>
2020-04-10 10:22:40 -0700

    add files for new design

---
+++ tutorials_cctbx/static/js/myjavascript_new.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+
+//    $(document).on('click', '.nav-item a', function (e) {
+//        $(this).parent().addClass('active').siblings().removeClass('active');
+//    });
+  var url = document.location.href;
+  var templ_name = document.location.pathname.split('/').slice(-1)[0]
+  if (templ_name!="") {
+    $("a[href='/']").removeClass("active");
+    $("a[href*="+templ_name+"]").addClass("active");
+
+  };
+
+  if (templ_name=='documentation_overview' || templ_name=='newsletter_artcls') {
+    //window.alert(templ_name)
+    //$('#body-row .collapse').collapse('show');
+    //$(".submenu-head").attr("aria-expanded":"true");
+    $("#submenu-doc").addClass("show");
+  };
+
+  var shown = false
+  $('.code').each(function(index, elem){
+      CodeMirror.fromTextArea(elem, {
+        mode: {name: "python",
+           version: 3,
+           singleLineStringErrors: false},
+        lineNumbers: true,
+        indentUnit: 4,
+        matchBrackets: true});
+  });
+  $('.output').each(function(index, elem){
+    CodeMirror.fromTextArea(elem, {
+      lineNumbers: false
+    })
+  })
+  var shown=true
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+
+    ind = $(e.target).parent().index()
+    if (ind==0 && shown==true) {
+      return;
+    }
+    shown=false
+    $('.CodeMirror').each(function(i, el){
+        el.CodeMirror.toTextArea();
+    });
+
+    $('.code').each(function(index, elem){
+        CodeMirror.fromTextArea(elem, {
+          mode: {name: "python",
+             version: 3,
+             singleLineStringErrors: false},
+          lineNumbers: true,
+          indentUnit: 4,
+          matchBrackets: true});
+    });
+    $('.output').each(function(index, elem){
+      CodeMirror.fromTextArea(elem, {
+        lineNumbers: false
+      })
+    })
+  });
+
+});
