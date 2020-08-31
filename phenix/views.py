4bcf04d8c01db308007d26d57129fbc1ab196a87
dcliebschner <dcliebschner@lbl.gov>
2020-08-31 11:32:55 -0700

    Phenix dev doc

--- phenix/views.py
+++ phenix/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'tutorials_phenix/index.html')
+  return render(request, 'phenix/index.html')
   #return HttpResponse("Hello, world. You're at the index.")
