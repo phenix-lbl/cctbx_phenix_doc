81030b56e17e7f123c310683926c7d7360795ff2
dcliebschner <dcliebschner@lbl.gov>
2020-09-15 10:48:52 -0700

    Use folder from phenix_dev_doc repository

--- phenix/views.py
+++ phenix/views.py
@@ -12,37 +12,37 @@ def doc_overview(request):
   return render(request, 'phenix/doc_overview.html')
 
 def doc_quick_reference(request):
-  return render(request, 'phenix/documentation/doc_quick_reference.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_quick_reference.html')
 
 def examples(request):
   return render(request, 'phenix/examples.html')
 
 def template(request):
-  return render(request, 'phenix/documentation/template.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/template.html')
 
 def doc_high_level_model_building_intro(request):
   return render(request, 'phenix/documentation/doc_high_level_model_building_intro.html')
 
 def doc_model_building_read_files(request):
-  return render(request, 'phenix/documentation/doc_model_building_read_files.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_read_files.html')
 
 def doc_fit_ligand(request):
-  return render(request, 'phenix/documentation/doc_fit_ligand.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_fit_ligand.html')
 
 def doc_model_building_1(request):
-  return render(request, 'phenix/documentation/doc_model_building_1.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_1.html')
 
 def doc_model_building_2(request):
-  return render(request, 'phenix/documentation/doc_model_building_2.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_2.html')
 
 def doc_model_building_3(request):
-  return render(request, 'phenix/documentation/doc_model_building_3.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_3.html')
 
 def doc_model_building_morphing(request):
-  return render(request, 'phenix/documentation/doc_model_building_morphing.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_morphing.html')
 
 def doc_model_building_sequence(request):
-  return render(request, 'phenix/documentation/doc_model_building_sequence.html')
+  return render(request, 'phenix/documentation/phenix_dev_doc_html/doc_model_building_sequence.html')
 
 
 
